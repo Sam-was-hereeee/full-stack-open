e5f2b3a454b1f6185595fce2e0f38130a9cfde30
Sam <lemon.bread5120@gmail.com>
2024-08-10 21:50:55 +0800

    refactored the code to contain three section each as react element

--- part1/course-introduction/src/App.jsx
+++ part1/course-introduction/src/App.jsx
@@ -1,3 +1,36 @@
+const Header = (props) =>{
+    return (
+            <>
+                <h1>{props.course}</h1>
+            </>
+        )
+
+}
+
+const Content = (props) => {
+    return(
+        <>
+            <p>
+                {props.content[0]} {props.exercise[0]}
+            </p>
+            <p>
+                {props.content[1]} {props.exercise[1]}
+            </p>
+            <p>
+                {props.content[2]} {props.exercise[2]}
+            </p>
+        </>
+    )
+}
+
+const Total = (props) =>{
+    return (
+        <>
+            <p>Number of exercises {props.total}</p>
+        </>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -6,20 +39,14 @@ const App = () => {
     const exercises2 = 7
     const part3 = 'State of a component'
     const exercises3 = 14
+    const exercises = [exercises1, exercises2, exercises3]
+    const parts = [part1, part2, part3]
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course}></Header>
+            <Content content={parts} exercise={exercises}></Content>
+            <Total total={exercises1+exercises2+exercises3}></Total>
         </div>
     )
 }
