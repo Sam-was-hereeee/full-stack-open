b0ab67381b2db2fba74a9d81d2de33d55a9acf87
Sam <lemon.bread5120@gmail.com>
2024-08-13 00:09:58 +0200

    Finished exercise 1.6, added 3 buttons and corresponding components to keep track of the reviews

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,44 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text}) => {
+    return (
+        <>
+            <button onClick={handleClick}>{text}</button>
+        </>
+    )
+};
+
+const Result = ({name, count}) => {
+    return (
+        <>
+            <p>{name}{count}</p>
+        </>
+    )
+}
+
 const App = () => {
     // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [review, setReview] = useState({
+        good: 0, neutral: 0, bad:0
+    });
+
+    const addReview = (reviewType) => {
+        const newReview = Object.assign({}, review);
+        newReview[reviewType] += 1;
+        setReview(newReview);
+    };
 
     return (
-        <div>
-            code here
-        </div>
+        <>
+            <h1>give feedback</h1>
+            <Button handleClick={()=>addReview('good')} text={"good"} />
+            <Button handleClick={()=>{addReview('neutral')}} text={'neutral'}/>
+            <Button handleClick={()=>{addReview('bad')}} text={'bad'}/>
+            <h1>statistics</h1>
+            <Result name={'good'} count={review.good}/>
+            <Result name={'neutral'} count={review.neutral}/>
+            <Result name={'bad'} count={review.bad}/>
+        </>
     )
 }
 
