f7017436bf75605625fafa149d2503cd515ed7fa
Sam <lemon.bread5120@gmail.com>
2024-08-14 17:29:39 +0200

    Connected the initialization of phonebooks to the local json-server used axios and an effect hook to fetch the data from local server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const SearchBar = ( {search, setSearch} ) => {
     return (
@@ -36,15 +37,20 @@ const PeopleDisplay = ({search, persons}) => {
 }
 
 const App = () => {
+    useEffect(()=>{
+        axios.get('http://localhost:3001/persons')
+            .then((res)=>{setPersons(res.data)})
+    }, [])
     const [persons, setPersons] = useState([
         {name: 'Arto Hellas', number: '040-123456', id: 1},
         {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3}
     ]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [search, setSearch] = useState('');
+
+
     const infoAndSet = {
         newName: newName,
         newNumber: newNumber,
