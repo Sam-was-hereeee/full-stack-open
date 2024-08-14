2ef5c5c136b63e1f7fad503eee16826f112c01a7
Sam <lemon.bread5120@gmail.com>
2024-08-14 16:34:31 +0200

    added search to the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '099999' }
-    ])
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ]);
+    const [newName, setNewName] = useState('');
+    const [newNumber, setNewNumber] = useState('');
+    const [search, setSearch] = useState('');
 
     const handleNewName = (e) => {
         e.preventDefault();
@@ -15,7 +19,9 @@ const App = () => {
         }
         const newPersonObj = {
             name: newName,
-            number: newNumber
+            number: newNumber,
+            id: persons.reduce((acc, el) =>
+                el.id>acc ? el.id+1 : acc)
         };
         setPersons(persons.concat(newPersonObj));
         setNewName('');
@@ -29,9 +35,13 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>Search: <input type="text" value={search} onChange={(e) => {
+                setSearch(e.target.value)
+            }}/></div>
+
             <form onSubmit={handleNewName}>
                 <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
+                name: <input value={newName} onChange={handleNameChange}/>
                 </div>
                 <div>number: <input type='number' value={newNumber} onChange={(e) =>
                 {setNewNumber(e.target.value)}}/></div>
@@ -40,7 +50,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map((person) => (<p key={person.name}>{person.name} {person.number}</p>))}
+            {persons.filter((el)=>el.name.toLowerCase().includes(search)).map((person) => (<p key={person.name}>{person.name} {person.number}</p>))}
         </div>
     )
 }
